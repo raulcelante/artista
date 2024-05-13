502f98d1d8f6dbbd63c92c8d01b65b1a905be5e3
raulcelante <169068428+raulcelante@users.noreply.github.com>
2024-05-13 15:13:21 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("#########");
+    System.out.println("# luan santana #");
+    System.out.println("#########");
+    System.out.println("");
+    System.out.println("album: viva ao vivo em salvador");
+    System.out.println("ano de lançamento: 2019");
+    System.out.println("");
+    System.out.println(" foi lançado em 22 de agosto de 2019");
+    System.out.println("");
+    System.out.println("######");
+    System.out.println("#musica#");
+    System.out.println("######");
+    System.out.println("choque térmico 2:54");
+    System.out.println("quando a bad bater 3:00");
+    System.out.println("sofrendo feito um louco 2:51");
+    System.out.println("ignore 3:37");
+    System.out.println("meu investimento 3:01");
+    System.out.println("mendigando amor 2:48");
+    System.out.println("água com açucar 4:28");
+    System.out.println("princesa não levanta 3:10");
+  }
+
+}
